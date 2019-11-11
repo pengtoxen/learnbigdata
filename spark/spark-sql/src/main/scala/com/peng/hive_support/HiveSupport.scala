d5e4c9f0bf11747431a9fbd41ce900059797a0a1
pengtoxen <82912094@qq.com>
2019-11-11 13:19:48 +0800

    update hive_support demo

--- spark/spark-sql/src/main/scala/com/peng/hive_support/HiveSupport.scala
+++ spark/spark-sql/src/main/scala/com/peng/hive_support/HiveSupport.scala
@@ -11,13 +11,22 @@ object HiveSupport extends App {
     val sparkConf: SparkConf = new SparkConf().setAppName("HiveSupport").setMaster("local[2]")
     //2 创建SparkSession对象
     val spark: SparkSession = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
-    //3 直接使用sparkSession去操作hivesql语句
+    //3 直接使用sparkSession去操作HiveSql语句
     //3.1 创建一张hive表
     spark.sql("create table people(name string,salary double,age int) row format delimited fields terminated by ','")
     //3.2 加载数据到hive表
-    spark.sql("load data local inpath './spark-sql/testdata/rawdata/saveResult/data.text' into table people")
+    spark.sql("load data local inpath 'spark-sql/testdata/rawdata/saveResult/data.text' into table people")
     //3.3 查询
+    /**
+      * 输出结果
+      * +-------+------+---+
+      * |   name|salary|age|
+      * +-------+------+---+
+      * |Michael|3000.0| 34|
+      * |   Andy|4500.0| 23|
+      * | Justin|3500.0| 55|
+      * +-------+------+---+
+      */
     spark.sql("select * from people").show()
-
     spark.stop()
 }
